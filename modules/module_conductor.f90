842fc89177440bb703ba7beaca3695cc75efe10f
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2015-07-14 07:32:27 +0200

    Skeleton of BVP_SOLVER calls is in place

--- modules/module_conductor.f90
+++ modules/module_conductor.f90
@@ -1,8 +1,11 @@
-! This
+! This module defines the data structure 'conductor', which models the physical state of a metallic 
+! conductor as a function of position and energy. The purpose of this structure is mainly to be used
+! as a base class for more interesting material classes, such as those that describe superconductors
+! and ferromagnets, or to be used in conjunction with such materials in hybrid structures.
 !
 ! Author:  Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
 ! Created: 2015-07-11
-! Updated: 2015-07-11
+! Updated: 2015-07-14
 
 module module_conductor
   use module_precision
@@ -12,14 +15,22 @@ module module_conductor
 
   ! Class declaration
   type conductor
-    type(state), allocatable :: state(:,:) ! Material state as a function of position and energy
-    type(state), allocatable :: bcl(:)     ! Boundary condition on the left  as a function of energy
-    type(state), allocatable :: bcr(:)     ! Boundary condition on the right as a function of energy
-    real(dp),    allocatable :: pos(:)     ! Which positions to consider
-    real(dp),    allocatable :: erg(:)     ! Which energies to consider
+    type(state), allocatable :: state(:,:) ! Physical state as a function of position and energy
+    type(state), allocatable :: bcl(:)     ! Boundary condition on the left  as function of energy
+    type(state), allocatable :: bcr(:)     ! Boundary condition on the right as function of energy
+    real(dp),    allocatable :: pos(:)     ! Discretized position domain 
+    real(dp),    allocatable :: erg(:)     ! Discretized energy domain 
+
+    real(dp)  :: interface_left  = inf     ! Interface parameter ζ at the left  interface
+    real(dp)  :: interface_right = inf     ! Interface parameter ζ at the right interface
+    real(dp)  :: thouless        = 1.0_dp
+    real(dp)  :: epsilon         = 0.001_dp
 
     contains
-    final      :: conductor_destruct      ! Class destructor
+    final     :: conductor_destruct       ! Class destructor
+    procedure :: update => conductor_update
+    !TODO class(conductor), pointer :: material_left, material_right
+    !TODO procedure Connect_Left / Connect_Right
   end type
 
   ! Class constructor
@@ -30,8 +41,8 @@ contains
   pure function conductor_construct_bcs(pos, erg, gap, eps) result(this)
     ! Constructs a conductor object corresponding to a BCS superconductor with a given position and energy range
     type(conductor)                   :: this     ! Conductor object that will be constructed
-    real(dp),    intent(in)           :: pos(:)   ! Array of positions where the material is to defined
-    real(dp),    intent(in)           :: erg(:)   ! Array of energies where the material is to be defined
+    real(dp),    intent(in)           :: pos(:)   ! Discretized position domain
+    real(dp),    intent(in)           :: erg(:)   ! Discretized energy domain
     real(dp),    intent(in), optional :: eps      ! Imaginary energy contribution (models inelastic scattering)
     complex(dp), intent(in), optional :: gap      ! Superconducting order parameter
 
@@ -85,5 +96,84 @@ contains
       deallocate(this%bcl)
       deallocate(this%bcr)
     end if
+  end subroutine
+
+  subroutine conductor_update(this)
+    ! This subroutine solves the Usadel equation numerically for the given position
+    ! and energy range, and updates the current estimate for the state of the system.
+    use bvp_m
+
+    class(conductor), intent(inout) :: this
+    real(dp)                        :: y(32,size(this%pos))
+    type(bvp_sol)                   :: sol
+    integer                         :: n, m
+
+    ! Loop over quasiparticle energies
+    do n=1,size(this%erg)
+      ! Status information
+      write (*,'(a,1x,i3,1x,a,1x,i3,1x,a,2x,f8.5)') '[',n,'/',ubound(this%erg,1),']',this%erg(n)
+
+      ! Convert all states at this energy level to real-valued state vectors
+      forall (m=1:size(this%pos))
+        y(:,m) = this%state(n,m)
+      end forall
+
+      ! Initialize the boundary value problem solver (TODO: Provide energy as parameter, compare with LUB)
+      sol = bvp_init(32, 16, this%pos, y)
+
+      ! Solve the differential equation
+      sol = bvp_solver(sol, ode, bc, method=6, error_control=1, tol=1.0e-6_dp, trace=2)
+
+      ! Use the results to update the current state of the system
+      forall (m=1:size(this%pos))
+        this%state(n,m) = sol%y(:,m)
+      end forall
+    end do
+  contains
+    subroutine ode(t, y, f)
+      ! Definition of the differential equation y'=f(t,y)
+      real(dp), intent(in)  :: t
+      real(dp), intent(in)  :: y(32)
+      real(dp), intent(out) :: f(32)
+
+      type(state)           :: s
+      type(spin)            :: g, gt, dg, dgt, N, Nt, d2g, d2gt
+
+      ! Convert the state vector to a state object
+      s = y
+
+      ! Extract the Riccati parameters
+      g   = s%g
+      gt  = s%gt
+      dg  = s%dg
+      dgt = s%dgt
+
+      ! Calculate the normalization matrices
+      N   = spin_inv( pauli0 - g*gt )
+      Nt  = spin_inv( pauli0 - gt*g )
+
+      ! Calculate the second derivatives of the Riccati parameters according to the Usadel equation TODO fix erg
+      d2g  = (-2.0_dp) * dg*Nt*gt*dg - ((0.0_dp,2.0_dp)/this%thouless) * cmplx(this%erg(1),this%epsilon,kind=dp)*g;
+      d2gt = (-2.0_dp) * dgt*N*g*dgt - ((0.0_dp,2.0_dp)/this%thouless) * cmplx(this%erg(1),this%epsilon,kind=dp)*gt;
+            
+      ! Pack the results into a state object
+      s%g   = dg
+      s%gt  = dgt
+      s%dg  = d2g
+      s%dgt = d2gt
+
+      ! Convert the state object to a state vector
+      f = s
+    end subroutine
+
+    subroutine bc(ya,yb,bca,bcb)
+      real(dp), intent(in)  :: ya(32)
+      real(dp), intent(in)  :: yb(32)
+      real(dp), intent(out) :: bca(16)
+      real(dp), intent(out) :: bcb(16)
+
+      bca( 1:16) = ya(17:32)
+      bcb( 1:16) = yb(17:32)
+    end subroutine
   end subroutine
 end module
--- modules/module_precision.f90
+++ modules/module_precision.f90
@@ -8,7 +8,10 @@
 
 module module_precision
   use, intrinsic :: iso_fortran_env
-  integer, parameter :: sp = REAL32
-  integer, parameter :: dp = REAL64
-  integer, parameter :: qp = REAL128
+  integer,  parameter :: sp  = REAL32
+  integer,  parameter :: dp  = REAL64
+  integer,  parameter :: qp  = REAL128
+
+  real(dp), parameter :: inf = huge(1.0_dp)
+  real(dp), parameter :: pi  = atan(1.0_dp)*4.0_dp
 end module 
