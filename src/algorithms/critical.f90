d7e1633df9880c7a8e47033d309c5b1c5e1c6d9f
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2015-07-23 08:43:25 +0200

    Moved the entire algorithm from src/tests/test_critical.f90 to src/algorithms/critical.f90.

--- src/algorithms/critical.f90
+++ src/algorithms/critical.f90
@@ -7,114 +7,118 @@ module mod_critical
   use mod_multilayer
   implicit none
 contains
-  subroutine critical_temperature(material, bisections, iterations, lower, upper, gap)
-    ! This subroutine calculates the critical temperature of a superconducting material using a binary search algorithm.
-    ! The two compulsory arguments are 'material', which is the superconducting material that we will find the critical
-    ! temperature of, and 'update', which is a subroutine that updates the current system state by solving the equations
-    ! of motion in the entire hybrid system. For more fine-grained control over the process, the user can also specify
-    ! the number of temperatures to check during the binary search (bisections), the number of updates to perform per
-    ! temperature until the state stabilizes (iterations), and what temperature range to investigate (lower to upper).
-    ! Finally, 'gap' determines the superconducting gap that is used to reinitialize the materials at all temperatures.
+subroutine critical_temperature(material, bisections, iterations, lower, upper, gap)
+  ! This subroutine calculates the critical temperature of a superconducting material using a binary search algorithm.
+  ! The two compulsory arguments are 'material', which is the superconducting material that we will find the critical
+  ! temperature of, and 'update', which is a subroutine that updates the current system state by solving the equations
+  ! of motion in the entire hybrid system. For more fine-grained control over the process, the user can also specify
+  ! the number of temperatures to check during the binary search (bisections), the number of updates to perform per
+  ! temperature until the state stabilizes (iterations), and what temperature range to investigate (lower to upper).
+  ! Finally, 'gap' determines the superconducting gap that is used to reinitialize the materials at all temperatures.
 
-    ! Declare data types for input variables
-    class(superconductor), target,   intent(inout) :: material
-    integer,               optional, intent(in   ) :: bisections
-    integer,               optional, intent(in   ) :: iterations
-    real(dp),              optional, intent(in   ) :: lower 
-    real(dp),              optional, intent(in   ) :: upper
-    complex(dp),           optional, intent(in   ) :: gap
+  ! Declare data types for input variables
+  class(superconductor), target,   intent(inout) :: material
+  integer,               optional, intent(in   ) :: bisections
+  integer,               optional, intent(in   ) :: iterations
+  real(dp),              optional, intent(in   ) :: lower 
+  real(dp),              optional, intent(in   ) :: upper
+  complex(dp),           optional, intent(in   ) :: gap
 
-    ! Declare data types for internal variables
-    integer                                        :: bisections_ = 12
-    integer                                        :: iterations_ =  5
-    real(dp)                                       :: lower_      =  0.0000_dp
-    real(dp)                                       :: upper_      =  1.0000_dp
-    complex(dp)                                    :: gap_        =  0.0001_dp
-    integer                                        :: n, m
+  ! Declare data types for internal variables
+  integer                                        :: bisections_ = 12
+  integer                                        :: iterations_ =  5
+  real(dp)                                       :: lower_      =  0.0000_dp
+  real(dp)                                       :: upper_      =  1.0000_dp
+  complex(dp)                                    :: gap_        =  0.0001_dp
+  integer                                        :: n, m
 
-    ! Handle optional arguments
-    call process_arguments
+  ! Handle optional arguments
+  call process_arguments
 
-    ! Perform the binary search
-    do n = 1,bisections_
-      ! Initialize a weakly superconducting state
-      call initialize_all(material, gap_)
-      call material%set_gap(gap_)
+  ! Set the system temperature to the midpoint of the search space
+  call material%set_temperature( (upper_ + lower_)/2.0_dp )
 
-      ! Update the state of the multilayer structure
-      do m = 1,iterations_
-        call update_all(material)
-        print *,n,m
-      end do
+  ! Perform the binary search
+  do n = 1,bisections_
+    ! Status information
+    call print_information
 
-      ! Check whether the gap has increased or not, and update the search space accordingly
-      if (abs(material%get_gap_mean()/gap_) >= 1.0_dp) then
-        lower_ = material%get_temperature()
-      else
-        upper_ = material%get_temperature()
-      end if
+    ! Initialize a weakly superconducting state
+    call initialize_all(material, gap_)
+    call material%set_gap(gap_)
 
-      ! Set the system temperature to the midpoint of the updated search space
-      call material%set_temperature( (upper_ + lower_)/2.0_dp )
+    ! Update the state of the multilayer structure
+    do m = 1,iterations_
+      call update_all(material)
     end do
-  contains
-    subroutine process_arguments
-      ! This nested subroutine processes the optional arguments to the parent subroutine.
-      if (present(bisections)) then
-        bisections_ = bisections
-      end if
 
-      if (present(iterations)) then
-        iterations_ = iterations
-      end if
+    ! Check whether the gap has increased or not, and update the search space accordingly
+    if (abs(material%get_gap_mean()/gap_) >= 1.0_dp) then
+      lower_ = material%get_temperature()
+    else
+      upper_ = material%get_temperature()
+    end if
 
-      if (present(lower)) then
-        lower_ = lower
-      end if
+    ! Set the system temperature to the midpoint of the updated search space
+    call material%set_temperature( (upper_ + lower_)/2.0_dp )
+  end do
 
-      if (present(upper)) then
-        upper_ = upper
-      end if
+  ! Print the final results
+  call print_results
+contains
+  subroutine process_arguments
+    ! This nested subroutine processes the optional arguments to the parent subroutine.
+    if (present(bisections)) then
+      bisections_ = bisections
+    end if
 
-      if (present(gap)) then
-        gap_ = gap
-      end if
-    end subroutine
+    if (present(iterations)) then
+      iterations_ = iterations
+    end if
+
+    if (present(lower)) then
+      lower_ = lower
+    end if
+
+    if (present(upper)) then
+      upper_ = upper
+    end if
+
+    if (present(gap)) then
+      gap_ = gap
+    end if
   end subroutine
 
-  !subroutine critical_bilayer(s, m, iterations, stabilization)
-  !  class(superconductor), intent(inout) :: s
-  !  class(conductor),      intent(inout) :: m
-  !  integer,               intent(in   ) :: iterations
-  !  integer,               intent(in   ) :: stabilization
-  !
-  !  real(dp)                             :: gap   = 0.001_dp
-  !  real(dp)                             :: lower = 0.000_dp
-  !  real(dp)                             :: upper = 1.000_dp
-  !  integer                              :: n
-  !
-  !  do n=1,iterations
-  !    ! Reset both materials to a weakly superconducting state
-  !    call s%initialize( gap = cmplx(gap,0.000_dp,kind=dp) )
-  !    call m%initialize( gap = cmplx(gap,0.000_dp,kind=dp) )
-  !
-  !    ! Set the temperature to the midpoint of the current search space
-  !    s%temperature = (lower + upper)/2.0_dp
-  !
-  !    ! Update the state of the hybrid system
-  !    !do m=1,stabilization
-  !      call m%update
-  !      call s%update
-  !    !  call update(s)
-  !    !end do
-  !
-  !    ! Check whether the gap has increased, and update the search space
-  !    if (abs(s%get_gap_mean()) >= gap) then
-  !      lower = s%temperature
-  !    else
-  !      upper = s%temperature
-  !    end if
-  !  end do
-  !end subroutine
+  subroutine print_information
+    ! Determine how much CPU time has elapsed
+    real(sp) :: time
+    call cpu_time(time)
 
+    ! Print the progress information to standard out
+    write(*,'(a)') '                                     '
+    write(*,'(a)') '╒═══════════════════════════════════╕'
+    write(*,'(a)') '│       PROGRESS  INFORMATION       │'
+    write(*,'(a)') '├───────────────────────────────────┤'
+    write(*,'(a,6x,a,i2.2,a,i2.2,7x,a)')                &
+      '│','Binary search: ', n, ' / ', bisections,     '│'
+    write(*,'(a,6x,a,f8.6,6x,a)')                       &
+      '│','Temperature:   ',material%get_temperature(),'│'
+    write(*,'(a,6x,a,i2.2,a,i2.2,a,i2.2,6x,a)')         &
+      '│','Elapsed time:  ',                            &
+      int(time/3600.0_sp),':',                          &
+      int(mod(time,3600.0_sp)/60.0_sp),':',             &
+      int(mod(time,60.0_sp)),                          '│'
+    write(*,'(a)') '╘═══════════════════════════════════╛'
+  end subroutine
+
+  subroutine print_results
+    write(*,*)
+    write(*,'(a)') '╒═══════════════════════════════════╕'
+    write(*,'(a)') '│           FINAL RESULTS           │'
+    write(*,'(a)') '├───────────────────────────────────┤'
+    write(*,'(a)') '│       Critical temperature:       │'
+    write(*,'(a,8x,f18.16,9x,a)') '│', material%get_temperature(), '│'
+    write(*,'(a)') '╘═══════════════════════════════════╛'
+  end subroutine
+end subroutine
 end module
--- src/tests/test_critical.f90
+++ src/tests/test_critical.f90
@@ -1,97 +1,26 @@
 ! This script calculates the critical temperature of a bulk superconductor, by performing a binary search for the
-! temperature where the gap vanishes numerically. The result should be numerically one in the given unit system.
+! temperature where the gap vanishes numerically. The result should be numerically one in the default unit system.
 !
 ! Written by Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
 ! Created 2015-07-21
-! Updated 2015-07-22
+! Updated 2015-07-23
 
 program test_critical
-  use mod_conductor
   use mod_superconductor
   use mod_multilayer
   use mod_critical
   implicit none
 
-  type(superconductor) :: s                           ! Superconductor
-  real(dp)             :: erg(600)                    ! Energy array
-  real(dp)             :: coupling      =  0.2000_dp  ! BCS coupling constant
-  real(dp)             :: scattering    =  0.0100_dp  ! Imaginary energy term due to inelastic scattering
-  complex(dp)          :: gap           =  0.0001_dp  ! Initial superconducting gap (relative to the zero-temperature bulk value)
-  real(dp)             :: lower         =  0.0000_dp  ! Lower limit for the critical temperature (relative to the bulk value)
-  real(dp)             :: upper         =  1.5000_dp  ! Upper limit for the critical temperature (relative to the bulk value)
-  real(dp)             :: length        = 25.0000_dp  ! Length of the superconductor (relative to the correlation length)
-  integer              :: iterations    = 25          ! Number of iterations of the binary search
-  integer              :: stabilization =  2          ! Number of iterations per temperature
-  integer              :: n, m                        ! Loop variables
+  type(superconductor) :: material
+  real(dp)             :: energy(600)
+  integer              :: n, m
 
   ! Initialize the energy array
-  call energy_range_positive(erg, coupling)
+  call energy_range_positive(energy, coupling = 0.200_dp)
 
   ! Initialize the superconductor
-  s = superconductor(erg, gap = gap, coupling = coupling, thouless = 1/length**2, scattering = scattering)
-  call s%set_temperature( (upper+lower)/2.0_dp )
+  material = superconductor(energy, coupling = 0.200_dp, thouless = 0.001_dp, scattering = 0.001_dp)
 
   ! Perform the binary search for the critical temperature
-  call critical_temperature(s, bisections = 10, iterations = 2, lower = 0.0_dp, upper = 1.5_dp, gap = (0.0001_dp,0.0000_dp))
-
-!  ! Perform the binary search for the critical temperature
-!  do n=1,iterations
-!    ! Initialize a weakly superconducting state
-!    call s%initialize( gap )
-!    call s%set_gap( gap )
-!
-!    ! Print status information
-!    call print_information
-!
-!    ! Update the state of the superconductor
-!    do m=1,stabilization
-!      write(*,'(a,i0,a,i0,a)') ' :: Updating superconductor [',m,'/',stabilization, ']'
-!      call s%update
-!    end do
-!
-!    ! Check whether the mean gap has increased, and update the temperature bounds accordingly
-!    if (abs(s%get_gap_mean()/gap) >= 1.0_dp) then
-!      lower = s%get_temperature()
-!    else
-!      upper = s%get_temperature()
-!    end if
-!
-!    ! Update the superconductor temperature based on the new bounds
-!    call s%set_temperature( (upper+lower)/2.0_dp )
-!  end do
-!
-!  ! Print final results
-!  call print_results
-contains
-  subroutine print_information
-    ! Determine how much CPU time has elapsed
-    real(sp) :: time
-    call cpu_time(time)
-
-    ! Print the progress information to standard out
-    write(*,*)
-    write(*,'(a)') '╒═══════════════════════════════════╕'
-    write(*,'(a)') '│       PROGRESS  INFORMATION       │'
-    write(*,'(a)') '├───────────────────────────────────┤'
-    write(*,'(a,6x,a,i2.2,a,i2.2,7x,a)')                &
-      '│','Binary search: ', n, ' / ', iterations,     '│'
-    write(*,'(a,6x,a,f8.6,6x,a)')                       &
-      '│','Temperature:   ', s%get_temperature(),      '│'
-    write(*,'(a,6x,a,i2.2,a,i2.2,a,i2.2,6x,a)')         &
-      '│','Elapsed time:  ',                            &
-      int(time/3600.0_sp),':',                          &
-      int(mod(time,3600.0_sp)/60.0_sp),':',             &
-      int(mod(time,60.0_sp)),                          '│'
-    write(*,'(a)') '╘═══════════════════════════════════╛'
-  end subroutine
-
-  subroutine print_results
-    write(*,*)
-    write(*,'(a)') '╒═══════════════════════════════════╕'
-    write(*,'(a)') '│           FINAL RESULTS           │'
-    write(*,'(a)') '├───────────────────────────────────┤'
-    write(*,'(a)') '│       Critical temperature:       │'
-    write(*,'(a,8x,f18.16,9x,a)') '│', s%get_temperature(), '│'
-    write(*,'(a)') '╘═══════════════════════════════════╛'
-  end subroutine
+  call critical_temperature(material, bisections = 10, iterations = 2, lower = 0.0_dp, upper = 1.5_dp)
 end program
