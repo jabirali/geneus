ed00f7118b6ab8f17f2f4bfbb71d2b8f05d44791
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2015-07-29 19:45:42 +0200

    Modified test_materials to test an SNF system

--- src/tests/test_materials.f
+++ src/tests/test_materials.f
@@ -13,7 +13,7 @@ program test_materials
 
   s = superconductor(erg, thouless = 1/1.0_dp**2, coupling  = 0.2_dp)
   c = conductor     (erg, thouless = 1/0.2_dp**2, spinorbit = spinorbit_xy(2.0_dp,pi/4))
-  f = ferromagnet   (erg, thouless = 1/0.5_dp**2, exchange  = exchange_xy(3.0_dp,-pi/4))
+  f = ferromagnet   (erg, thouless = 1/0.2_dp**2, exchange  = exchange_xy(3.0_dp,-pi/4))
 
   call connect(s, c, 0.33_dp, 0.33_dp)
   call connect(c, f, 0.33_dp, 0.33_dp)
@@ -27,41 +27,41 @@ program test_materials
   !call calculate_dos(s, iterations=2, unit=1)
 
   open(newunit=u, file='test_materials.dat') ! Status (Old? New? Replace?) and position (append?) and action (write?)
-  call f%write_dos(u, 0.0_dp, 1.0_dp)
-  call s%write_dos(u, 1.0_dp, 2.0_dp)
-  call c%write_dos(u, 2.0_dp, 3.0_dp)
+  call s%write_dos(u, 0.0_dp, 1.0_dp)
+  call c%write_dos(u, 1.0_dp, 2.0_dp)
+  call f%write_dos(u, 2.0_dp, 3.0_dp)
   close(unit=u)
  
   do n=1,3
     call f%update
          open(newunit=u, file='test_materials.dat') ! Status (Old? New? Replace?) and position (append?) and action (write?)
-         call f%write_dos(u, 0.0_dp, 1.0_dp)
-         call s%write_dos(u, 1.0_dp, 2.0_dp)
-         call c%write_dos(u, 2.0_dp, 3.0_dp)
+         call s%write_dos(u, 0.0_dp, 1.0_dp)
+         call c%write_dos(u, 1.0_dp, 2.0_dp)
+         call f%write_dos(u, 2.0_dp, 3.0_dp)
          close(unit=u)
     call c%update
          open(newunit=u, file='test_materials.dat') ! Status (Old? New? Replace?) and position (append?) and action (write?)
-         call f%write_dos(u, 0.0_dp, 1.0_dp)
-         call s%write_dos(u, 1.0_dp, 2.0_dp)
-         call c%write_dos(u, 2.0_dp, 3.0_dp)
+         call s%write_dos(u, 0.0_dp, 1.0_dp)
+         call c%write_dos(u, 1.0_dp, 2.0_dp)
+         call f%write_dos(u, 2.0_dp, 3.0_dp)
          close(unit=u)
     call s%update
          open(newunit=u, file='test_materials.dat') ! Status (Old? New? Replace?) and position (append?) and action (write?)
-         call f%write_dos(u, 0.0_dp, 1.0_dp)
-         call s%write_dos(u, 1.0_dp, 2.0_dp)
-         call c%write_dos(u, 2.0_dp, 3.0_dp)
+         call s%write_dos(u, 0.0_dp, 1.0_dp)
+         call c%write_dos(u, 1.0_dp, 2.0_dp)
+         call f%write_dos(u, 2.0_dp, 3.0_dp)
          close(unit=u)
     call c%update
          open(newunit=u, file='test_materials.dat') ! Status (Old? New? Replace?) and position (append?) and action (write?)
-         call f%write_dos(u, 0.0_dp, 1.0_dp)
-         call s%write_dos(u, 1.0_dp, 2.0_dp)
-         call c%write_dos(u, 2.0_dp, 3.0_dp)
+         call s%write_dos(u, 0.0_dp, 1.0_dp)
+         call c%write_dos(u, 1.0_dp, 2.0_dp)
+         call f%write_dos(u, 2.0_dp, 3.0_dp)
          close(unit=u)
   end do
 
   open(newunit=u, file='test_materials.dat') ! Status (Old? New? Replace?) and position (append?) and action (write?)
-  call f%write_dos(u, 0.0_dp, 1.0_dp)
-  call s%write_dos(u, 1.0_dp, 2.0_dp)
-  call c%write_dos(u, 2.0_dp, 3.0_dp)
+  call s%write_dos(u, 0.0_dp, 1.0_dp)
+  call c%write_dos(u, 1.0_dp, 2.0_dp)
+  call f%write_dos(u, 2.0_dp, 3.0_dp)
   close(unit=u)
 end program
