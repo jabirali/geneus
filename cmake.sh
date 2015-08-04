b5310d833d8703c4b03c0aeef815dd5db102cc39
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2015-08-04 09:46:56 +0200

    Updated the cmake.sh script to work on standard Ubuntu systems with GCC5 installed, and renamed it to configure.

--- cmake.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -r debug release
-cmake -DCMAKE_Fortran_COMPILER=/opt/gcc5/bin/gfortran -DCMAKE_C_COMPILER=/opt/gcc5/bin/gcc -DCMAKE_CXX_COMPILER=/opt/gcc5/bin/g++ -DCMAKE_BUILD_TYPE=Release -H. -Brelease
-cmake -DCMAKE_Fortran_COMPILER=/opt/gcc5/bin/gfortran -DCMAKE_C_COMPILER=/opt/gcc5/bin/gcc -DCMAKE_CXX_COMPILER=/opt/gcc5/bin/g++ -DCMAKE_BUILD_TYPE=Debug   -H. -Bdebug
