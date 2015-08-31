87db7d57b652b9ada9e878f4a5232f92d93b9075
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2015-08-31 11:38:18 +0200

    Removed Gphi distributions.

--- matlab/ballistic/diffcond_plot.m
+++ matlab/ballistic/diffcond_plot.m
@@ -1,10 +1,14 @@
-function diffcond_plot(transmission, polarization, scattering, temperature)
+function diffcond_plot(transmission, polarization, phaseshift, scattering, temperature)
   % Calculates and plots the differential conductance as function of voltage for
   % a ballistic superconductor/normal-metal bilayer with a spin-active tunneling
-  % interface. The temperature, transmission probability, spin-polarization, and
-  % inelastic scattering rate must be specified as arguments. The temperature is
-  % normalized to the superconducting critical temperature Tc, and the inelastic
-  % scattering is an imaginary energy term normalized to the superconducting gap.
+  % interface between a superconductor and normal metal. 
+  %
+  % The input parameters are:
+  %   transmission  --  Interfacial transmission (range [ 0,1])                     [scalar]
+  %   polarization  --  Interfacial polarization (range [-1,1])                     [scalar]
+  %   phaseshift    --  Spin-dependent interfacial phase shift (normalized to pi)   [scalar]
+  %   scattering    --  Inelastic scattering rate (normalized to the zero-temp gap) [scalar]
+  %   temperature   --  Temperature of the system (normalized to the critical temp) [vector]
 
 
 
@@ -15,7 +19,6 @@ function diffcond_plot(transmission, polarization, scattering, temperature)
   % Parameters to use in calculations
   voltage = linspace(-3,+3,200);
   energy  = linspace(-6,+6,400);
-  angle   = linspace(0,pi/2,1000);
 
   % Spin-dependent transmission probabilities
   D_up = transmission * (1+polarization);
@@ -44,27 +47,21 @@ function diffcond_plot(transmission, polarization, scattering, temperature)
     gap = tanh(1.74*sqrt(1/(temperature(k)+1e-16)-1));
 
     % Calculate the spectral current density as a function of energy
-    for a = 1:length(angle)
-      phaseshift  = pi * exp(-4*sin(angle(a))^2);
-      probability = cos(angle(a)) * (range(angle)/length(angle));
-      for m=1:length(energy)
-        if (abs(energy(m)) < gap)
-          % Calculation below the superconducting gap
-          % [eqs. (45) and (50) in PRB 70 134510]
-          delta         = acos((energy(m)+i*scattering)/gap);
-          spectral(m,k) = spectral(m,k)                       ...
-                        + probability                         ...
-                        * ( 1/(B+C*cos(2*delta+pi*phaseshift))...
-                          + 1/(B+C*cos(2*delta-pi*phaseshift)) );
-        else
-          % Calculation above the superconducting gap
-          % [eqs. (46) and (49) in PRB 70 134510]
-          delta         = acosh((abs(energy(m))+i*scattering)/gap);
-          spectral(m,k) = spectral(m,k)                                            ...
-                        + probability                                              ...
-                        * ( 2*cosh(delta)*(A*exp(-delta) + 2*sinh(delta))          ...
-                          / (exp(2*delta) + U*exp(-2*delta) + V*cos(pi*phaseshift)) );
-        end
+    for m=1:length(energy)
+      if (abs(energy(m)) < gap)
+        % Calculation below the superconducting gap
+        % [eqs. (45) and (50) in PRB 70 134510]
+        delta         = acos((energy(m)+i*scattering)/gap);
+        spectral(m,k) = spectral(m,k)                     ...
+                      + 1/(B+C*cos(2*delta+pi*phaseshift))...
+                      + 1/(B+C*cos(2*delta-pi*phaseshift));
+      else
+        % Calculation above the superconducting gap
+        % [eqs. (46) and (49) in PRB 70 134510]
+        delta         = acosh((abs(energy(m))+i*scattering)/gap);
+        spectral(m,k) = spectral(m,k)                                        ...
+                      + 2*cosh(delta)*(A*exp(-delta) + 2*sinh(delta))        ...
+                      / (exp(2*delta) + U*exp(-2*delta) + V*cos(pi*phaseshift));
       end
     end
   end
@@ -82,8 +79,8 @@ function diffcond_plot(transmission, polarization, scattering, temperature)
       for m=1:length(energy)
         % Calculate the total current density as a function of voltage bias
         % [eq.(51) in PRB 70 134510, where 0.8819... is half the BCS ratio]
-        current(n,k) = current(n,k)                                                            ...
-                     + real(spectral(m,k)) * step                                              ...
+        current(n,k) = current(n,k)                                                                         ...
+                     + spectral(m,k) * step                                                                 ...
                      * (tanh(0.8819384944310228*(energy(m)+voltage(n))/(temperature(k)+1e-16)) ...
                        -tanh(0.8819384944310228*(energy(m)-voltage(n))/(temperature(k)+1e-16)))/4;
       end 
@@ -99,7 +96,7 @@ function diffcond_plot(transmission, polarization, scattering, temperature)
   % Calculate the differential conductance as a function of voltage
   diffcond = zeros(length(voltage)-1,length(temperature));
   for k=1:length(temperature)
-    diffcond(:,k) = diff(current(:,k)) ./ diff(voltage(:));
+    diffcond(:,k) = diff(real(current(:,k))) ./ diff(voltage(:));
   end
 
   % Plot the differential conductance (overlay)
@@ -109,7 +106,7 @@ function diffcond_plot(transmission, polarization, scattering, temperature)
   xlabel('Voltage eV/\Delta_0');
   ylabel('Differential conductance R·dI/dV');
   legend(strtrim(cellstr(strcat('T=',num2str(temperature')))))
-  axis([min(voltage) max(voltage) -2 4]);
+  axis([min(voltage) max(voltage) min(0,min(diffcond)) 4]);
 
   % Plot the differential conductance (gradient)
   figure;
