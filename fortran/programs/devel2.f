b71e1b62ffd388eaf3e4c636d5a627ae8e1072e8
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2016-03-07 14:39:32 +0100

    The new structure class, which will replace the old multilayer class, is now able to construct new materials in the linked list based on a string, update all the material layers in the correct sequence, and each material can be configured using key-value string pairs. The foundation is now in place for a transition to a config-file based simulation.

--- fortran/programs/devel2.f
+++ fortran/programs/devel2.f
@@ -1,19 +1,74 @@
 module mod_structure
   use mod_hybrid
+  use mod_stdio
 
   type :: structure
-    class(material), allocatable :: top
+    class(material), pointer :: a => null()
+    class(material), pointer :: b => null()
   contains
+    procedure :: push   => structure_push
     procedure :: update => structure_update
   end type
 contains
+  impure subroutine structure_push(struct, string)
+    !! Constructs a new class(material) object at the bottom of the multilayer stack.
+    class(structure), intent(inout) :: struct
+    character(*),     intent(in   ) :: string
+
+    if (.not. associated(struct % b)) then
+      ! This is the first layer in the structure
+      call material_allocate(struct % b, string)
+      call material_construct(struct % b)
+      struct % a => struct % b
+    else
+      ! This is not the first layer in the structure
+      call material_allocate(struct % b % material_b, string)
+      call material_construct(struct % b % material_b)
+      struct % b % material_b % material_a => struct % b
+      struct % b => struct % b % material_b
+    end if
+  contains
+    impure subroutine material_allocate(ptr, str)
+      !! Allocates memory for a new material layer.
+      class(material), pointer, intent(inout) :: ptr
+      character(*),             intent(in   ) :: str
+
+      select case(str)
+        case('ferromagnet')
+          allocate(ferromagnet    :: ptr)
+        case('superconductor')
+          allocate(superconductor :: ptr)
+        case('conductor')
+          allocate(conductor      :: ptr)
+        case default
+          call error('Material type "' // str // '" unknown!')
+      end select
+    end subroutine
+
+    impure subroutine material_construct(ptr)
+      !! Constructs and initializes a new material layer.
+      class(material), pointer, intent(inout) :: ptr
+
+      select type(ptr)
+        type is (ferromagnet)
+          ptr = ferromagnet(30.0_wp, exchange = [10.0_wp,0.0_wp,0.0_wp])
+        type is (superconductor)
+          ptr = superconductor(30.0_wp)
+        type is (conductor)
+          ptr = conductor(30.0_wp)
+        class default
+          call error('Attempted to construct an unsupported material type!')
+      end select
+    end subroutine
+  end subroutine
+
   impure subroutine structure_update(s)
     ! This subroutine updates the state of the entire hybrid structure.
     class(structure), target  :: s
     class(material),  pointer :: p
 
     ! Initialize the material pointer to the top of the stack
-    p => s % top
+    p => s % a
 
     ! Update all materials (going down)
     do while (associated(p % material_b))
@@ -32,30 +87,23 @@ end module
 program test
   use mod_hybrid
   use mod_material
-  use mod_neoconf
+  use mod_structure
 
-  type(superconductor) :: s
-  type(ferromagnet)    :: f
+  type(structure) :: bilayer
 
-  s = superconductor(30.0_wp)
-  f = ferromagnet(30.0_wp)
-  call connect(s,f)
+  call bilayer % push('superconductor')
+  call bilayer % push('ferromagnet')
 
-  call f%conf('temperature', '0.10')
-  call f%conf('scattering',  '0.05')
-  call f%conf('length',      '0.50')
+  call bilayer % a % conf('temperature', '0.10')
+  call bilayer % a % conf('scattering',  '0.05')
+  call bilayer % a % conf('length',      '0.75')
+  call bilayer % a % conf('coupling',    '0.25')
 
-  call s%conf('temperature', '0.10')
-  call s%conf('scattering',  '0.05')
-  call s%conf('length',      '0.75')
-  call s%conf('coupling',    '0.25')
+  call bilayer % b % conf('temperature', '0.10')
+  call bilayer % b % conf('scattering',  '0.05')
+  call bilayer % b % conf('length',      '0.50')
 
-  write(*,*) f%thouless
-  write(*,*) f%temperature
-  write(*,*) f%scattering
-
-  call f%update
-  call s%update
+  call bilayer % update
 end program
 
 
