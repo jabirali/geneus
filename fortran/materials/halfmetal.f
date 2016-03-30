7ce039b8100a8b7c9e6eb6ea6d69441012132927
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2016-03-30 13:31:55 +0200

    Started implementing halfmetals more seriously.

--- fortran/materials/halfmetal.f
+++ fortran/materials/halfmetal.f
@@ -1,5 +1,5 @@
 ! This module defines the data type 'halfmetal', which models the physical state of a strong or halfmetallic ferromagnet.
-! @TODO: Finish this module.
+! The type is a member of class(conductor), and inherits the internal structure and generic methods defined there.
 !
 ! Author:  Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
 ! Created: 2016-03-08
@@ -17,19 +17,16 @@ module mod_halfmetal
 
   ! Type declarations
   type, public, extends(conductor) :: halfmetal
-    ! These parameters control the physical characteristics of the material
-    real(wp)                  :: polarization            =  0.99_wp                        ! Spin-polarization of the ferromagnet
+    real(wp)            :: polarization =  0.00_wp                                ! Spin-polarization of the ferromagnet
+    type(spin), private :: P                                                      ! Polarization matrix
   contains
-    ! These methods are required by the class(material) abstract interface
-    procedure                 :: init                    => halfmetal_init                 ! Initializes the propagators
-    procedure                 :: diffusion_equation      => halfmetal_diffusion_equation   ! Defines the Usadel diffusion equation
-    procedure                 :: interface_equation_a    => halfmetal_interface_equation_a ! Boundary condition at the left  interface
-    procedure                 :: interface_equation_b    => halfmetal_interface_equation_b ! Boundary condition at the right interface
-    procedure                 :: update_prehook          => halfmetal_update_prehook       ! Code to execute before calculating the propagators
-    procedure                 :: update_posthook         => halfmetal_update_posthook      ! Code to execute after  calculating the propagators
-
-    ! These methods define miscellaneous utility functions
-    procedure                 :: conf                    => halfmetal_conf                 ! Configures material parameters
+    procedure           :: init                 => halfmetal_init                 ! Initializes the propagators
+    procedure           :: diffusion_equation   => halfmetal_diffusion_equation   ! Defines the Usadel diffusion equation
+    procedure           :: interface_equation_a => halfmetal_interface_equation_a ! Boundary condition at the left  interface
+    procedure           :: interface_equation_b => halfmetal_interface_equation_b ! Boundary condition at the right interface
+    procedure           :: update_prehook       => halfmetal_update_prehook       ! Code to execute before calculating the propagators
+    procedure           :: update_posthook      => halfmetal_update_posthook      ! Code to execute after  calculating the propagators
+    procedure           :: conf                 => halfmetal_conf                 ! Configures the material parameters
   end type
 
   ! Type constructors
@@ -81,43 +78,39 @@ contains
     type(spin)                      :: N, Nt
     type(spin)                      :: P
 
-    ! Ensure that the Riccati parameters are diagonal
-    h   % matrix(1,2) = 0
-    h   % matrix(2,1) = 0
-    ht  % matrix(1,2) = 0
-    ht  % matrix(2,1) = 0
-    dh  % matrix(1,2) = 0
-    dh  % matrix(2,1) = 0
-    dht % matrix(1,2) = 0
-    dht % matrix(2,1) = 0
-
-    ! Calculate the polarization matrix
-    P   % matrix(1,1) = 2/(1 + eps + this%polarization)
-    P   % matrix(1,2) = 0
-    P   % matrix(2,1) = 0
-    P   % matrix(2,2) = 2/(1 + eps - this%polarization)
-
     ! Calculate the normalization matrices
     N   = spin_inv( pauli0 - h*ht )
     Nt  = spin_inv( pauli0 - ht*h )
 
     ! Calculate the second-derivatives of the Riccati parameters
-    d2g  = (-2.0_wp,0.0_wp)*dh*Nt*ht*dh - (0.0_wp,2.0_wp)*e*P*h
-    d2gt = (-2.0_wp,0.0_wp)*dht*N*h*dht - (0.0_wp,2.0_wp)*e*P*ht
+    associate(P => this % P)
+      d2g  = (-2.0_wp,0.0_wp)*dh*Nt*ht*dh - (0.0_wp,2.0_wp)*e*P*h
+      d2gt = (-2.0_wp,0.0_wp)*dht*N*h*dht - (0.0_wp,2.0_wp)*e*P*ht
+    end associate
   end subroutine
 
   pure subroutine halfmetal_interface_equation_a(this, a, g, gt, dg, dgt, r, rt)
-      ! Calculate residuals from the boundary conditions at the left interface.
-      class(halfmetal),          intent(in   ) :: this
-      type(green),               intent(in   ) :: a
-      type(spin),                intent(in   ) :: g, gt, dg, dgt
-      type(spin),                intent(inout) :: r, rt
+    ! Calculate residuals from the boundary conditions at the left interface.
+    class(halfmetal),          intent(in   ) :: this
+    type(green),               intent(in   ) :: a
+    type(spin),                intent(in   ) :: g, gt, dg, dgt
+    type(spin),                intent(inout) :: r, rt
 
-    ! Calculate the deviation between the materials
-    associate(g0  => a%g, gt0 => a%gt)
-      r  = g  - g0
-      rt = gt - gt0
-    end associate
+    ! Diagonal components: use spin-active boundary conditions
+    if (associated(this%material_a)) then
+      ! Interface is tunneling
+      call this%interface_tunnel_a(a, g, gt, dg, dgt, r, rt)
+      call this%interface_spinactive_a(a, g, gt, dg, dgt, r, rt)
+    else
+      ! Interface is vacuum
+      call this%interface_vacuum_a(g, gt, dg, dgt, r, rt)
+    end if
+
+    ! Off-diagonal components: use boundary conditions g,gt=0
+    r  % matrix(1,2)  = g  % matrix(1,2)
+    r  % matrix(2,1)  = g  % matrix(2,1)
+    rt % matrix(1,2)  = gt % matrix(1,2)
+    rt % matrix(2,1)  = gt % matrix(2,1)
   end subroutine
 
   pure subroutine halfmetal_interface_equation_b(this, b, g, gt, dg, dgt, r, rt)
@@ -127,38 +120,79 @@ contains
     type(spin),                intent(in   ) :: g, gt, dg, dgt
     type(spin),                intent(inout) :: r, rt
 
-    ! Calculate the deviation between the materials
-    associate(g3 => b%g, gt3 => b%gt)
-      r  = g  - g3
-      rt = gt - gt3
-    end associate
+    ! Diagonal components: use spin-active boundary conditions
+    if (associated(this%material_b)) then
+      ! Interface is tunneling
+      call this%interface_tunnel_b(b, g, gt, dg, dgt, r, rt)
+      call this%interface_spinactive_b(b, g, gt, dg, dgt, r, rt)
+    else
+      ! Interface is vacuum
+      call this%interface_vacuum_b(g, gt, dg, dgt, r, rt)
+    end if
+
+    ! Off-diagonal components: use boundary conditions g,gt=0
+    r  % matrix(1,2)  = g  % matrix(1,2)
+    r  % matrix(2,1)  = g  % matrix(2,1)
+    rt % matrix(1,2)  = gt % matrix(1,2)
+    rt % matrix(2,1)  = gt % matrix(2,1)
   end subroutine
 
   impure subroutine halfmetal_update_prehook(this)
     ! Code to execute before running the update method of a class(halfmetal) object.
     class(halfmetal), intent(inout) :: this
 
+    ! Verify that a polarization is defined
+    if (abs(this % polarization) < eps) then
+      call error('Tried to update a halfmetal with no defined polarization!')
+    end if
+
+    ! Update the polarization matrix
+    this % P % matrix(1,1) = 2/(1 + eps + this%polarization)
+    this % P % matrix(1,2) = 0
+    this % P % matrix(2,1) = 0
+    this % P % matrix(2,2) = 2/(1 + eps - this%polarization)
+
     ! Update the left  interface parameters
-    this % magnetization_a = [0,0,1] * sign(1.0_wp, this % polarization)
-    this % polarization_a  = abs(this % polarization)
+    this % polarization_a  = this % polarization
+    this % magnetization_a = [0,0,1]
 
     ! Update the right interface parameters
-    this % magnetization_b = [0,0,1] * sign(1.0_wp, this % polarization)
-    this % polarization_b  = abs(this % polarization)
+    this % magnetization_b = [0,0,1]
+    this % polarization_b  = this % polarization
 
     ! Call the superclass prehook
-    call this%conductor%update_posthook
+    call this%conductor%update_prehook
 
     ! Modify the type string
-    this%type_string = color_purple // 'HALFMETAL' // color_none
+    this%type_string = color_red // 'HALFMETAL' // color_none
   end subroutine
 
   impure subroutine halfmetal_update_posthook(this)
     ! Code to execute after running the update method of a class(halfmetal) object.
     class(halfmetal), intent(inout) :: this
+    real(wp)                        :: error
+    integer                         :: n, m
 
     ! Call the superclass posthook
     call this%conductor%update_posthook
+
+    ! Perform sanity checks
+    error = 0
+    do m=1,size(this%location)
+      do n=1,size(this%energy)
+        ! Find the maximum off-diagonal element
+        error = max(abs(this%propagator(n,m)%g  % matrix(1,2)), error)
+        error = max(abs(this%propagator(n,m)%g  % matrix(2,1)), error)
+        error = max(abs(this%propagator(n,m)%gt % matrix(1,2)), error)
+        error = max(abs(this%propagator(n,m)%gt % matrix(2,1)), error)
+      end do
+    end do
+
+    ! Status information
+    if (this%information >= 0 .and. .not. this % lock) then
+      write(stdout,'(4x,a,f10.8,a)') 'Max error:  ',error,'                                        '
+      flush(stdout)
+    end if
   end subroutine
 
   !--------------------------------------------------------------------------------!
@@ -170,10 +204,21 @@ contains
     class(halfmetal), intent(inout) :: this
     character(*),     intent(in   ) :: key
     character(*),     intent(in   ) :: val
+    real(wp)                        :: tmp
 
     select case(key)
       case ('polarization')
         read(val,*) this % polarization
+      case ('conductance_a')
+        call evaluate(val, this % conductance_a)
+      case ('conductance_b')
+        call evaluate(val, this % conductance_b)
+      case ('resistance_a')
+        call evaluate(val, tmp)
+        this % conductance_a = 1/tmp
+      case ('resistance_b')
+        call evaluate(val, tmp)
+        this % conductance_b = 1/tmp
       case default
         call material_conf(this, key, val)
     end select
