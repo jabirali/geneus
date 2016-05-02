d70b8c7613f302dd1d62011cc2eead5201b16437
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2016-05-02 09:00:35 +0200

    Made plotting script configfile-driven.

--- python/plot.py
+++ python/plot.py
@@ -6,37 +6,32 @@ import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+# Set the matplotlib stylesheet
 mpl.style.use('babaplot.mplstyle')
 
-
-# What to read and plot -- get this from user somehow
-ax_lab = { 'x': 'Horizontal axis',
-           'y': 'Vertical axis'  }
-
-# Read config file
+# Read and parse a config file
 config = configparser.ConfigParser()
 config.read('plot.conf')
 
 files = {filename: columns.replace(' ','').split(',') for filename, columns in config['files'].items()}
-
-x_data = config['plots']['x'].replace(' ','')
-y_data = config['plots']['y'].replace(' ','').split(',')
+plots = config['plots']
+axes  = config['axes']
 
 # Read and concatenate input files into one dataframe
 df = pd.concat((pd.read_table(key, names=files[key]) for key in sorted(files)), axis=1)
 
-# Define the expressions that will be plotted
-df.eval('_x = ' + x_data, inplace=True)
+# Plot the datasets
+for label, data in plots.items():
+    # Define the expression to plot
+    x_data, y_data = data.replace(' ','').split(',')
+    df.eval('_x = ' + x_data, inplace=True)
+    df.eval('_y = ' + y_data, inplace=True)
 
-# Perform the plotting using matplotlib
-for y in y_data:
-    df.eval('_y = ' + y, inplace=True)
-    plt.plot('_x', '_y', data=df)
-    #df.eval('_y = ' + y, inplace=True)
-    #plt.plot('_x', '_y', data=df, label=label)
+    # Perform the actual plotting
+    plt.plot('_x', '_y', data=df, label=label)
 
 # Finalize
-plt.xlabel(ax_lab['x'])
-plt.ylabel(ax_lab['y'])
-plt.legend(ncol=len(y_data))
+plt.xlabel(axes.get('x', ''))
+plt.ylabel(axes.get('y', ''))
+plt.legend(ncol=5)
 plt.show()
