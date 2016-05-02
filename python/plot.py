4c449c51568268670a6a9b7ec15ac54bbc6ba2da
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2016-05-02 08:10:17 +0200

    Experimenting with config file parsing.

--- python/plot.py
+++ python/plot.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import configparser
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 mpl.style.use('babaplot.mplstyle')
 
-# What to read and plot -- get this from user somehow
-files = {'critical_0.000.dat': ['a', 'b', 'c'],
-         'critical_0.500.dat': ['u', 'v', 'w']}
 
+# What to read and plot -- get this from user somehow
 ax_lab = { 'x': 'Horizontal axis',
            'y': 'Vertical axis'  }
 
-x_data = 'a'
-y_data = { 'c':   'c data',
-           'c-w': 'c-w data'}
+# Read config file
+config = configparser.ConfigParser()
+config.read('plot.conf')
+
+files = {filename: columns.replace(' ','').split(',') for filename, columns in config['files'].items()}
+
+x_data = config['plots']['x'].replace(' ','')
+y_data = config['plots']['y'].replace(' ','').split(',')
 
 # Read and concatenate input files into one dataframe
 df = pd.concat((pd.read_table(key, names=files[key]) for key in sorted(files)), axis=1)
@@ -25,9 +29,11 @@ df = pd.concat((pd.read_table(key, names=files[key]) for key in sorted(files)), 
 df.eval('_x = ' + x_data, inplace=True)
 
 # Perform the plotting using matplotlib
-for y, label in y_data.items():
+for y in y_data:
     df.eval('_y = ' + y, inplace=True)
-    plt.plot('_x', '_y', data=df, label=label)
+    plt.plot('_x', '_y', data=df)
+    #df.eval('_y = ' + y, inplace=True)
+    #plt.plot('_x', '_y', data=df, label=label)
 
 # Finalize
 plt.xlabel(ax_lab['x'])
