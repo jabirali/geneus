fe7285d3c8df3d41e9a60a37396a19018737cf7e
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2017-03-07 15:42:18 +0100

    Progress.

--- matlab/fourier/dynesfulton.m
+++ matlab/fourier/dynesfulton.m
@@ -1,35 +1,20 @@
-function dynesfulton(inputfile, outputfile)
+function c = dynesfulton(inputfile, outputfile)
   % Load input file
   data    = load(inputfile);
   field   = data(:,1);
-  current = data(:,2);
-  N       = length(field);
+  current = abs(data(:,2));
 
-  % Estimate the first derivative
-  d1 = zeros(N,1);
-  for n=(1+1):(N-1)
-    d1(n) = (current(n+1)-current(n-1))/(field(n+1)-field(n-1));
-  end
-  d1(1) = (current(1+1)-current(1))/(field(1+1)-field(1));
-  d1(N) = (current(N)-current(N-1))/(field(N)-field(N-1));
+  % Estimate the local curvature
+  c = curvature(current, field);
 
-  % TODO: Estimate the second derivative
-  % TODO: Estimate the curvature c=f''(x)/f(x)
-  % TODO: Use c>0 as an indicator for sign-change — in the fraunhofer
-  %       case, we typically get c>>0 when approaching a sign change from
-  %       the center, c<<0 when approaching from outside, c=-10 normally.
+  % Identify spikes in the curvature
+  u = spikes(c);
 
-  % Create the indicator function
-  % TODO: Make this more robust using med+mad to find d1 spikes
-  u = ones(N,1);
-  for n=2:N
-    if (d1(n) > 0 && d1(n-1) < 0)
-      u(n:N) = -u(n:N);
-    end
-  end
+  % Create a sign array that flips after each spike
+  s = (-1) .^ cumsum(u);
 
-  % Multiply by the indicator
-  current = current .* u;
+  % Use this to restore the sign of the measured current
+  current = s .* current;
 
   % Make sure the central peak is a maximum
   if (abs(max(current)) < abs(min(current)))
@@ -41,6 +26,66 @@ function dynesfulton(inputfile, outputfile)
   fourier = ifft(field)
 
   % Dump processed output
-  data = [field, current];
+  %data = [field, u];
+  data = u;
   save(outputfile, 'data', '-ascii');
+end
+
+function u = spikes(y)
+  % Uses an outlier-detection algorithm to identify abberant maxima in a dataset.
+
+  % Find the median and median-absolute-deviation of the input array
+  N = length(y);
+  m = median(y);
+  s = mad(y,1);
+
+  % Identify regions that exceed a statistical threshold value
+  u = y > m + 3*s;
+
+  % Check where the outlier regions start and stop
+  %d  = differentiate1(u, (1:N)');
+  %u1 = find(d > 0);
+  %u2 = find(d < 0);
+
+  %um = mean([u1, u2]')';
+
+  %um = u;
+end
+
+function c = curvature(y, x)
+  % Calculates the local curvature of some function y(x), using a finite-difference
+  % approximation for the numerical derivatives dy/dx and d²y/dx² of the arrays.
+
+  % Calculate the first and second derivatives
+  d1 = differentiate1(y, x);
+  d2 = differentiate2(y, x);
+
+  % Calculate the signed local curvature 
+  c = d2 ./ (1+d1.^2).^(3/2);
+end
+
+function d = differentiate1(y, x)
+  % Calculates the first-derivative dy/dx, using a central-difference approximation in 
+  % the interior domain, and a forward/backward-difference approximation at the edges.
+
+  % Initialize variables
+  N = min(length(x),length(y));
+  d = zeros(N,1);
+
+  % Calculate the finite-difference first-derivative
+  d(  1  ) = (y( 2 )-y(  1  )) ./ (x( 2 )-x(  1  ));
+  d(2:N-1) = (y(3:N)-y(1:N-2)) ./ (x(3:N)-x(1:N-2));
+  d(  N  ) = (y( N )-y( N-1 )) ./ (x( N )-x( N-1 ));
+end
+
+function d = differentiate2(y, x)
+  % Calculates the second-derivative d²y/dx², using a central-difference approximation
+  % in the interior domain of the function y(x), and skipping calculation at the edges.
+
+  % Initialize variables
+  N = min(length(x),length(y));
+  d = zeros(N,1);
+
+  % Calculate the finite-difference second-derivative
+  d(2:N-1) = (y(3:N) - 2*y(2:N-1) + y(1:N-2)) ./ (x(3:N) - x(1:N-2)).^2;
 end
