fc9f21710d06cbe53e3a7a2cd3670640b79823b0
Jabir Ali Ouassou <jabir.a.ouassou@ntnu.no>
2022-01-19 16:44:13 +0100

    clean: manually reformatted `math.f90`

--- src/foundation/math.f90
+++ src/foundation/math.f90
@@ -35,20 +35,18 @@ module math_m
     !------------------------------------------------------------
 
     interface trace
-    !! Public interface for functions that calculate a matrix trace.
-        module procedure matrix_trace ! Trace of a matrix
+    !!  Public interface for functions that calculate a matrix trace.
+        module procedure matrix_trace
     end interface
 
     interface inverse
-    !! Public interface for functions that calculate a matrix inverse.
-        module procedure matrix_inverse_re, & ! Inverse of a real matrix
-            matrix_inverse_cx ! Inverse of a complex matrix
+    !!  Public interface for functions that calculate a matrix inverse.
+        module procedure matrix_inverse_re, matrix_inverse_cx
     end interface
 
     interface diag
-    !! Public interface for functions that deal with matrix diagonals.
-        module procedure matrix_diag, & ! Construct a diagonal matrix
-            vector_diag ! Extract a matrix diagonal
+    !!  Public interface for functions that deal with matrix diagonals.
+        module procedure matrix_diag, vector_diag
     end interface
 
     !------------------------------------------------------------
@@ -56,37 +54,31 @@ module math_m
     !------------------------------------------------------------
 
     interface mean
-    !! Public interface for routines that calculate the mean value.
-        module procedure mean_array_re, & ! Mean of a real array
-            mean_array_cx ! Mean of a complex array
+    !!  Public interface for routines that calculate the mean value.
+        module procedure mean_array_re, mean_array_cx
     end interface
 
     interface differentiate
-    !! Public interface for various differentiation routines.
-        module procedure differentiate_array_re, & ! Derivative of a real array
-            differentiate_array_cx ! Derivative of a complex array
+    !!  Public interface for various differentiation routines.
+        module procedure differentiate_array_re, differentiate_array_cx
     end interface
 
     interface integrate
-    !! Public interface for various integration routines.
-        module procedure integrate_array_re, & ! Trapezoid integration of a real array
-            integrate_array_cx, & ! Trapezoid integration of a complex array
-            integrate_range_re, & ! Interpolated integration of a real array
-            integrate_range_cx ! Interpolated integration of a complex array
+    !!  Public interface for various integration routines.
+        module procedure integrate_array_re, integrate_array_cx, &
+                         integrate_range_re, integrate_range_cx
     end interface
 
     interface interpolate
-    !! Public interface for various interpolation routines.
-        module procedure interpolate_point_re, & ! Interpolate a scalar function from a real array to a point
-            interpolate_point_cx, & ! Interpolate a scalar function from a complex array to a point
-            interpolate_array_re, & ! Interpolate a scalar function from a real array to an array
-            interpolate_array_cx, & ! Interpolate a scalar function from a complex array to an array
-            interpolate_point_matrix_re ! Interpolate a matrix function from a real array array to a point
+    !!  Public interface for various interpolation routines.
+        module procedure interpolate_point_re, interpolate_point_cx, &
+                         interpolate_array_re, interpolate_array_cx, &
+                         interpolate_point_matrix_re
     end interface
 
     interface linspace
-    !! Public interface for routines that initialize arrays.
-        module procedure linspace_array_re ! Linearly spaced real numbers
+    !!  Public interface for routines that initialize arrays.
+        module procedure linspace_array_re
     end interface
 
 end module
